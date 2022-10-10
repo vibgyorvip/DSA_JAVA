8f3d8c1f7ba884e2949ed9f49ff4005506b96f40
Vipul Agnihotri <86738105+vibgyorvip@users.noreply.github.com>
2022-10-11 03:17:38 +0530

    Create Main.java

---
+++ ArrayEX/SingleDimensionArray/Main.java
@@ -0,0 +1,15 @@
+package programming;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SIngleDimensionArray arr = new SIngleDimensionArray(5);
+		arr.insert(0, 1);
+		arr.insert(1, 2);
+		arr.insert(2, 3);
+		arr.insert(1, 5);
+		arr.insert(10, 17);
+	}
+
+}
