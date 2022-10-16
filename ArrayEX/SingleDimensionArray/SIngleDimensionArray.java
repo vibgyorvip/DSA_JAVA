263f6bc9aa56ef534afe53c33db305ad21f74003
Vipul Agnihotri <86738105+vibgyorvip@users.noreply.github.com>
2022-10-16 16:47:44 +0530

    Update SIngleDimensionArray.java

--- ArrayEX/SingleDimensionArray/SIngleDimensionArray.java
+++ ArrayEX/SingleDimensionArray/SIngleDimensionArray.java
@@ -13,6 +13,7 @@ public class SIngleDimensionArray {
 		
 	}
 
+	// inserting the element to the particular location
 	public void insert(int location , int valueToBeInserted)
 	{
 		try
@@ -20,11 +21,11 @@ public class SIngleDimensionArray {
 			if(intArray[location] == Integer.MIN_VALUE)
 			{
 				intArray[location]= valueToBeInserted;
-				System.out.println("Element"+valueToBeInserted+"Inserted successfully");
+				System.out.println("Element "+valueToBeInserted+" Inserted successfully");
 			}
 			else
 			{
-				System.out.println("This location number"+location+"is already occupied");
+				System.out.println("This location number "+location+" is already occupied");
 			}
 		}
 		catch(ArrayIndexOutOfBoundsException e)
@@ -34,6 +35,47 @@ public class SIngleDimensionArray {
 	}
 	
 	
+	// Array Traversal
+	public void traverseArray() 
+	{
+		try {
+			for(int index =0 ; index < intArray.length ; index++)
+			{
+				System.out.println(intArray[index]+" ");
+			}
+		}catch(Exception e)
+		{
+			System.out.println("Array no longer exists");
+		}
+		
+	}
 	
+	// Search an element in an Array
+	public void searchInArray(int valueToSearch)
+	{
+		for(int index =0 ; index < intArray.length ; index++)
+		{
+			if(intArray[index] == valueToSearch)
+			{
+				System.out.println("Element found at postion " + index);
+				return;
+			}
+		}
+		System.out.println(valueToSearch + " element not found");
+	}
+	
+	// delete value from array
+	public void deleteValue(int valueToDeleteIndex) 
+	{
+		try
+		{	
+			intArray[valueToDeleteIndex] = Integer.MIN_VALUE; 
+			System.out.println("The value deleted at postion " +valueToDeleteIndex+" successfully");
+		}catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("The value that is provided not in the range of array");
+		}
+		
+	}
 
 }
