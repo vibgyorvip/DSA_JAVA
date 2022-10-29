ebb3d8525abcec92b1f430e569a5ede884ae946f
Vipul Agnihotri <86738105+vibgyorvip@users.noreply.github.com>
2022-10-29 16:57:06 +0530

    Update Attempt1.java

--- ArrayEX/ArrayInterviewQuestion/FindNumberInArray/Attempt1.java
+++ ArrayEX/ArrayInterviewQuestion/FindNumberInArray/Attempt1.java
@@ -6,12 +6,12 @@ public class FindNumberInArray {
 		// TODO Auto-generated method stub
 		FindNumberInArray obj= new FindNumberInArray();
 		int[] arr = {1,3,5,7,8};
-		int result=obj.searchInArray(arr, 5);
+		int result=obj.linearSearch(arr, 5);
 		System.out.println("Number found at position : "+result);
 		
 	}
 	
-        public int searchInArray(int[] intArray, int valueToSearch) {
+        public int linearSearch(int[] intArray, int valueToSearch) {
             // TODO
             for(int index =0 ; index<intArray.length ; index++)
             {
